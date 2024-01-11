7e3b3f1d8aabaa524a3ecb725124a970c7e9dc0f
Elena <elenarosamail@gmail.com>
2024-01-11 10:13:19 -0800

    wip

---
+++ src/componants/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import CoffeeList from "./CoffeeList";
+
+
+function App(){
+  return ( 
+    <React.Fragment>
+      <Header />
+      <CoffeeList />
+    </React.Fragment>
+  );
+}
+
+export default App;
